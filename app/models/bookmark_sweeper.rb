a529d6d9e34b657b31c8e15705d7e2b6c34a30d7
Kosuke Tanabe <tanabe@mwr.mediacom.keio.ac.jp>
2011-10-10 04:58:08 +0900

    added sweeper

--- app/models/bookmark_sweeper.rb
+++ app/models/bookmark_sweeper.rb
@@ -1,10 +1,17 @@
 class BookmarkSweeper < ActionController::Caching::Sweeper
   include ExpireEditableFragment
-  observe Bookmark
+  observe Bookmark, Tag
 
   def after_save(record)
-    expire_editable_fragment(record.manifestation, ['show_list', 'detail'])
-    expire_tag_cloud(record)
+    case record
+    when record.is_a?(Bookmark)
+      expire_editable_fragment(record.manifestation, ['show_list', 'detail'])
+      expire_tag_cloud(record)
+    when record.is_a?(Tag)
+      record.taggings.collect(&:taggable).each do |taggable|
+        expire_editable_fragment(taggable)
+      end
+    end
   end
 
   def after_destroy(record)
