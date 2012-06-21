37153e202a2d0fd255fbf72ca8706e3f95ae7c10
Kosuke Tanabe <tanabe@mwr.mediacom.keio.ac.jp>
2012-06-22 03:14:50 +0900

    added suggest_tag

--- lib/enju_bookmark.rb
+++ lib/enju_bookmark.rb
@@ -2,6 +2,7 @@ require "enju_bookmark/engine"
 require "enju_bookmark/bookmark_url"
 require "enju_bookmark/calculate_stat"
 require "enju_bookmark/expire_tag_cloud"
+require "enju_bookmark/suggest_tag"
 
 module EnjuBookmark
 end
