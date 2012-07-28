f66bb3396921c41100f11c4c14052f1443cd5401
Kosuke Tanabe <tanabe@mwr.mediacom.keio.ac.jp>
2012-07-29 00:40:18 +0900

    updated generator

--- lib/generators/enju_bookmark/views_generator.rb
+++ lib/generators/enju_bookmark/views_generator.rb
@@ -1,4 +1,4 @@
-module EnjuLibrary
+module EnjuBookmark
   module Generators
     class ViewsGenerator < Rails::Generators::Base
       source_root File.expand_path('../../../../app/views', __FILE__)
