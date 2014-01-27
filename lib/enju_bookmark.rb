0a53fb2b514c3eb8444b019b1c79922fa87906ce
Kosuke Tanabe <kosuke@e23.jp>
2014-01-27 10:52:44 +0900

    cleaned up

--- lib/enju_bookmark.rb
+++ lib/enju_bookmark.rb
@@ -4,8 +4,8 @@ require "enju_bookmark/manifestation"
 require "enju_bookmark/bookmark_url"
 require "enju_bookmark/calculate_stat"
 require "enju_bookmark/expire_tag_cloud"
-#require "enju_bookmark/suggest_tag"
 require "enju_bookmark/bookmark_helper"
+#require "enju_bookmark/suggest_tag"
 
 module EnjuBookmark
 end
