2070f03ffe2f07e67f355aa694547fe5492d407b
Kosuke Tanabe <kosuke@e23.jp>
2014-03-01 13:51:34 +0900

    updated factory file

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -6,12 +6,12 @@ FactoryGirl.define do
     f.library_id{FactoryGirl.create(:library).id}
     f.password 'adminpassword'
     f.password_confirmation 'adminpassword'
-    f.user_group_id{UserGroup.where(:name => 'User').first.id}
-    f.required_role_id{Role.where(:name => 'User').first.id}
+    f.user_group_id{UserGroup.where(:name => 'user').first.id}
+    f.required_role_id{Role.find_by_name('User').id}
     f.locale 'ja'
     f.after(:create) do |user|
       user_has_role = UserHasRole.new
-      user_has_role.assign_attributes({:user_id => user.id, :role_id => Role.where(:name => 'Administrator').first.id})
+      user_has_role.assign_attributes({:user_id => user.id, :role_id => Role.find_by_name('Administrator').id})
       user_has_role.save
       user.reload
     end
@@ -24,12 +24,12 @@ FactoryGirl.define do
     f.library_id{FactoryGirl.create(:library).id}
     f.password 'librarianpassword'
     f.password_confirmation 'librarianpassword'
-    f.user_group_id{UserGroup.where(:name => 'User').first.id}
-    f.required_role_id{Role.where(:name => 'User').first.id}
+    f.user_group_id{UserGroup.where(:name => 'user').first.id}
+    f.required_role_id{Role.find_by_name('User').id}
     f.locale 'ja'
     f.after(:create) do |user|
       user_has_role = UserHasRole.new
-      user_has_role.assign_attributes({:user_id => user.id, :role_id => Role.where(:name => 'Librarian').first.id})
+      user_has_role.assign_attributes({:user_id => user.id, :role_id => Role.find_by_name('Librarian').id})
       user_has_role.save
       user.reload
     end
@@ -42,13 +42,13 @@ FactoryGirl.define do
     f.library_id{FactoryGirl.create(:library).id}
     f.password 'userpassword'
     f.password_confirmation 'userpassword'
-    f.user_group_id{UserGroup.where(:name => 'not_specified').first.id}
-    f.required_role_id{Role.where(:name => 'User').first.id}
+    f.user_group_id{UserGroup.where(:name => 'user').first.id}
+    f.required_role_id{Role.find_by_name('User').id}
     f.locale 'ja'
     f.sequence(:user_number){|n| "user_number_#{n}"}
     f.after(:create) do |user|
       user_has_role = UserHasRole.new
-      user_has_role.assign_attributes({:user_id => user.id, :role_id => Role.where(:name => 'User').first.id})
+      user_has_role.assign_attributes({:user_id => user.id, :role_id => Role.find_by_name('User').id})
       user_has_role.save
       user.reload
     end
