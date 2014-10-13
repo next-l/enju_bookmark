b9707d394abb7e733e1680179273f0e9090571f8
Kosuke Tanabe <nabeta@fastmail.fm>
2014-10-14 00:26:01 +0900

    updated dummy migration files

--- db/migrate/20140524142813_add_taggings_count_to_tag.rb
+++
@@ -1,5 +0,0 @@
-class AddTaggingsCountToTag < ActiveRecord::Migration
-  def change
-    add_column :tags, :taggings_count, :integer, default: 0
-  end
-end
--- lib/enju_bookmark/version.rb
+++ lib/enju_bookmark/version.rb
@@ -1,3 +1,3 @@
 module EnjuBookmark
-  VERSION = "0.1.2.pre18"
+  VERSION = "0.1.2.pre19"
 end
