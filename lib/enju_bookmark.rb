5bafc5374e54dc716acf13577585de5ad9598f7a
Kosuke Tanabe <nabeta@fastmail.fm>
2015-11-25 03:29:19 +0900

    move EnjuBookmark::EnjuUser module

--- lib/enju_bookmark.rb
+++ lib/enju_bookmark.rb
@@ -1,9 +1,7 @@
 require "enju_bookmark/engine"
-require "enju_bookmark/user"
 require "enju_bookmark/profile"
 require "enju_bookmark/manifestation"
 require "enju_bookmark/bookmark_url"
-require "enju_bookmark/calculate_stat"
 require "enju_bookmark/expire_tag_cloud"
 require "enju_bookmark/bookmark_helper"
 #require "enju_bookmark/suggest_tag"
@@ -11,6 +9,5 @@ require "enju_bookmark/bookmark_helper"
 module EnjuBookmark
 end
 
-ActiveRecord::Base.send :include, EnjuBookmark::BookmarkUser
 ActiveRecord::Base.send :include, EnjuBookmark::BookmarkProfile
 ActiveRecord::Base.send :include, EnjuBookmark::BookmarkManifestation
--- spec/dummy/app/models/user.rb
+++ spec/dummy/app/models/user.rb
@@ -4,6 +4,6 @@ class User < ActiveRecord::Base
     :recoverable, :rememberable, :trackable, #, :validatable
     :lockable, :lock_strategy => :none, :unlock_strategy => :none
 
-  enju_leaf_user_model
-  enju_bookmark_user_model
+  include EnjuLeaf::EnjuUser
+  include EnjuBookmark::EnjuUser
 end
